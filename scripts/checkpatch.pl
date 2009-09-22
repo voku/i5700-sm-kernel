dabff64a800bfcd53587103e910920757e0ea987
Andy Whitcroft <apw@canonical.com>
2009-09-22 08:04:38 +0800

    checkpatch: format strings should not have brackets in macros
    We should not recommend braces for the following:
    
        #define pr_fmt(fmt)    "%s: " fmt, __func__
    
    allow things with double quotes round them to avoid this check.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2287,7 +2287,8 @@ sub process {
 				DECLARE_PER_CPU|
 				DEFINE_PER_CPU|
 				__typeof__\(|
-				\.$Ident\s*=\s*
+				\.$Ident\s*=\s*|
+				^\"|\"$
 			}x;
 			#print "REST<$rest> dstat<$dstat>\n";
 			if ($rest ne '') {
