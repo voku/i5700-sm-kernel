9a66fd80432039cf0f3e7853b0c7f36bcfa0a63d
Borislav Petkov <borislav.petkov@amd.com>
2009-10-02 21:23:21 +0800

    initcalls: Add early_initcall() for modules
    Complete the early_initcall() API by making it available in modules
    too. To be used by the EDAC/MCE code.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andi Kleen <andi@firstfloor.org>
    LKML-Reference: <20091002132321.GC28682@aftab>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/init.h
+++ include/linux/init.h
@@ -274,6 +274,7 @@ void __init parse_early_param(void);
 #else /* MODULE */
 
 /* Don't use these in modules, but some people do... */
+#define early_initcall(fn)		module_init(fn)
 #define core_initcall(fn)		module_init(fn)
 #define postcore_initcall(fn)		module_init(fn)
 #define arch_initcall(fn)		module_init(fn)
